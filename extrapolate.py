e870972be23bc39882d75dce7f12536371c621e2
Hittento <dummypaltiera@gmail.com>
2023-04-27 15:32:29 +0530

    Database corrected for Pre-Computed use

--- extrapolate.py
+++ extrapolate.py
@@ -1,24 +1,30 @@
-def extrapolate_uptime_downtime(observations, business_hours):
-    """
-    :param observations: list of tuples containing observation time and status (up or down)
-    :param business_hours: tuple containing start and end time of business hours
-    :return: list of tuples containing time intervals and status (up or down)
-    """
-    start_time, end_time = business_hours
-    extrapolated_data = []
-    current_status = observations[0][1]
-    current_start_time = start_time
-    for observation in observations:
-        observation_time, status = observation
-        if status != current_status:
-            extrapolated_data.append((current_start_time, observation_time, current_status))
-            current_start_time = observation_time
-            current_status = status
-    extrapolated_data.append((current_start_time, end_time, current_status))
-    return extrapolated_data
-#
-# Example usage:
-observations = [('10:14', 'up'), ('11:15', 'down')]
-business_hours = ('9:00', '12:00')
-extrapolated_data = extrapolate_uptime_downtime(observations, business_hours)
-print(extrapolated_data)
+import pytz
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+from datetime import timezone,timedelta,datetime
+config = dotenv_values(".env")
+
+client = MongoClient("mongodb+srv://user:user@testdb.lz5fe4t.mongodb.net/?retryWrites=true&w=majority")
+db = client["LoopDB"]
+for x in client[config["DB_NAME"]]["rel-menu"].find():
+    # Time_Zone = client[config["DB_NAME"]]["rel-timezones"].find_one({"store_id":x["store_id"]})
+    # tmz = Time_Zone["timezone_str"]
+    start = x["start_time"]
+    end = x["end_time"]
+    myset = set()
+    for y in client[config["DB_NAME"]]['rel_times'].find({"store_id":x["store_id"]}):
+        temp = y["timestamp_utc"][0:10]
+        myset.add(temp)
+        if "2023-01-19" not in myset: myset.add
+        if "2023-01-20" not in myset:
+            client[config["DB_NAME"]]["rel_times"].insert_one({"store_id":y["store_id"],"timestamp_utc":start,"status":"active"})
+            client[config["DB_NAME"]]["rel_times"].insert_one(
+                {"store_id": y["store_id"], "timestamp_utc": end, "status": "active"})
+        if "2023-01-21" not in myset: myset.add("2023-01-21")
+        if "2023-01-22" not in myset: myset.add("2023-01-22")
+        if "2023-01-23" not in myset: myset.add("2023-01-23")
+        if "2023-01-24" not in myset: myset.add("2023-01-20")
+        if "2023-01-25" not in myset: myset.add("2023-01-20")
+
+    print(sorted(myset))
